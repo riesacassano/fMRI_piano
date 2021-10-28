502ad1d2e309db020176b0db315a0dd8c7e279fe
Riesa Cassano <rcassano@princeton.edu>
2021-10-28 17:04:08 -0400

    Able to plot now

--- ryc_102821_k_to_event_length.py
+++ ryc_102821_k_to_event_length.py
@@ -1,4 +1,4 @@
-# This script plots k against event length. It computes event lengths from all of the HMMs for average of AM subjects (saved in '../models/HMM_event_seg/'). All conditions, reps, and ROIs are included. This plot is simply to visualize the relationship between k and event length. 
+# This script plots k against median event length. It computes event lengths (EL) from all of the HMMs for average of AM subjects (saved in '../models/HMM_event_seg/'). All conditions, reps, and ROIs are included. This plot is simply to visualize the relationship between k and event length. SD of event lengths is also plotted. Average is not used, because average event length is just n_TRs / k. Median is actually informative.
 
 import numpy as np
 import pandas as pd
@@ -15,26 +15,53 @@ reps_per_scrambled_cond = 3
 reps_per_control_cond = 2
 k_set = np.array((2,3,4,5,8,10,12,15,18,20,22,25,30,35,40))
 
-for jasmine in range(1):#len(ROIs)):
+conversion_factor = 1.7 # 1TR = 1.7s
+
+avg_el_all = []
+sd_el_all = []
+
+for jasmine in range(len(ROIs)):
 	roi = ROIs[jasmine]
 
-	for c in range(1):#len(conds)):
+	for c in range(len(conds)):
 		cond = conds[c]
 
 		if cond == 'Listen': reps = reps_per_control_cond
 		else: reps = reps_per_scrambled_cond
 
-		for r in range(1):#reps):
+		for r in range(reps):
 			rep = 'rep%d'%(r+1)
 
 			# load the HMM event segments for this ROI, condition, and rep
 			this_HMM = pd.read_csv(input_filepath+'%s/%s_%s_%s'%(roi,group_label,cond,rep),index_col=0)
 
-			# for each k, compute average event length
-			this_avg_event_length = []
+			# for each k, compute average event length and SD of event lengths with conversion to seconds
+			this_avg_el = []
+			this_sd_el = []
 
-			#for k in k_set:
-			for trilby in range(1):
-				
-				events = this_HMM[k]
-				print(
+			#for trilby in range(3):
+			#	k = k_set[trilby]
+			for k in k_set:
+				events = this_HMM['%d'%k].to_numpy()
+				_,counts = np.unique(events,return_counts=True)
+				this_avg_el.append(np.median(counts)*conversion_factor)
+				this_sd_el.append(np.std(counts)*conversion_factor)
+
+			avg_el_all.append(this_avg_el)
+			sd_el_all.append(this_sd_el)
+
+avg_el_all = np.asarray(avg_el_all)
+sd_el_all = np.asarray(sd_el_all)
+
+avg_el_all = np.flip(avg_el_all,axis=1) # reverse the order of the columns for plotting
+sd_el_all = np.flip(sd_el_all,axis=1)
+
+#print(avg_el_all)
+
+fig,ax = plt.subplots(figsize=(7,7))
+ax.boxplot(avg_el_all,vert=False,labels=np.flip(k_set))
+#plt.show()
+
+fig,ax = plt.subplots(figsize=(7,7))
+ax.boxplot(sd_el_all,vert=False,labels=np.flip(k_set))
+plt.show()
