7347d0cfc2d4f00a64d5b0dc9109fb14b9841d8f
Riesa Cassano <rcassano@princeton.edu>
2021-11-02 21:59:37 -0400

    More informed k-sweep

--- ryc_102621_event_seg_subject_avg.py
+++ ryc_102621_event_seg_subject_avg.py
@@ -5,6 +5,7 @@ from scipy import stats
 from sklearn.preprocessing import StandardScaler
 import pandas as pd
 from brainiak.eventseg.event import EventSegment # make sure you're in virtual environment -> 'source activate venv'
+import time
 
 input_filepath = '../data/data_by_run/' # then ROI/subject/cond/repX.npy 
 output_filepath_WvA = '../data/HMM_WvA_scores/'
@@ -26,7 +27,8 @@ if group_label == 'M': subjects = M_subjects
 # all available ROIs
 # ROIs = ['AngularG', 'Cerebellum', 'HeschlsG', 'STG', 'MotorCortex', 'TPJ', 'PCC', 'Precuneus', 'A1', 'mPFC', 'Hipp', 'lTPJ', 'rTPJ', 'PMC', 'V1'] 
 # select ROIs
-ROIs = ['AngularG','MotorCortex','TPJ','A1','mPFC','Hipp','PMC', 'V1'] 
+# ROIs = ['AngularG','MotorCortex','TPJ','A1','mPFC','Hipp','PMC', 'V1'] 
+ROIs = ['MotorCortex','A1','PMC','V1']
 conds = ['1B','2B','8B','I','Listen']
 reps_per_scrambled_cond = 3
 reps_per_control_cond = 2
@@ -34,9 +36,15 @@ reps_per_control_cond = 2
 df_header = ['cond','rep']
 
 k_test = np.array((2,4,8))
-k_select = np.array((2,3,4,5,8,10,12,15,18,20,22,25,30,35,40)) # 15 values for k
+#k_select = np.array((2,3,4,5,8,10,12,15,18,20,22,25,30,35,40)) # 15 values for k
 # Jamal uses 10 values when he does the k sweep: (3,5,9,15,20,25,30,35,40,45)
 k_all = np.arange(2,41) # include all k values from 2 to 40
+
+k_low = np.arange(3,19)
+k_mid = np.arange(20,34,2)
+k_high = np.array((35,40,45,52,60))
+k_select = np.hstack((k_low,k_mid,k_high))
+
 k_set = k_select
 
 df_header = df_header + k_set.tolist()
@@ -57,6 +65,8 @@ for jasmine in range(len(ROIs)):
 
 		for r in range(reps):
 			rep = 'rep%d'%(r+1)
+
+			start = time.time()
 
 			# create an empty list to hold all subjects' data
 			all_data = []
@@ -106,7 +116,7 @@ for jasmine in range(len(ROIs)):
 				# compute within versus across score and append to the list for this subject/cond/rep
 				same_event = events[:,np.newaxis] == events
 				within = cc[same_event*local_mask].mean()
-				across = cc[(~same_event)*local_mask].mean() # "mean of empty slice"
+				across = cc[(~same_event)*local_mask].mean()
 				within_across = within - across
 				wva_this_run.append(within_across)
 
@@ -121,7 +131,9 @@ for jasmine in range(len(ROIs)):
 			# append WvA score list as a row to the big WvA list
 			wva_scores.append(this_wva_row)
 
-			print('%s, %s done'%(cond,rep))
+			end = time.time()
+
+			print('%s, %s done (%f seconds)'%(cond,rep,end-start))
 
 
 	# convert WvA scores to a DataFrame and save it for this ROI
